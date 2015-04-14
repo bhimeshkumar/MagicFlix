1ef4b5a912906ee9ff6dce307d165666eeaa5ba4
Bhimesh <bhimesh@generaldevelopers.com>
2015-04-14 14:01:40 +0530

    - Fixed issues with loading default profile avatars

--- MagikFlix-Kurio/src/com/magikflix/kurio/app/adapters/ChildProfileAdapter.java
+++ MagikFlix-Kurio/src/com/magikflix/kurio/app/adapters/ChildProfileAdapter.java
@@ -1,8 +1,6 @@
 package com.magikflix.kurio.app.adapters;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Vector;
 
 import android.app.Activity;
@@ -18,7 +16,6 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.localytics.android.LocalyticsAmpSession;
 import com.magikflix.kurio.MagikFlix;
 import com.magikflix.kurio.R;
 import com.magikflix.kurio.app.activities.FamilySetUpActivity;
@@ -26,25 +23,20 @@ import com.magikflix.kurio.app.api.Model.UserProfile;
 
 public class ChildProfileAdapter extends BaseAdapter{
 
-	private static String TAG = CategoryAdapter.class.getName();
+	//private static String TAG = CategoryAdapter.class.getName();
 	private Activity mContext;
 	private String[] mList;
 	int m_w, m_h;
 	protected Vector<Boolean> selectedStates;
-	private LocalyticsAmpSession mLocalyticsSession;
-	private Map<String, String> mCategorySelectorEvent ;
-	private int enableCategoryWidthHeight,disableCategoryWidthHeight;
+	private int disableCategoryWidthHeight;
 	private List<UserProfile> mUserProfileLsit;
 
 	public ChildProfileAdapter(Activity context, List<UserProfile> userProfiles) {
 		mUserProfileLsit = userProfiles;
-		mCategorySelectorEvent = new HashMap<String, String>();
 		mContext = context;
-		mLocalyticsSession = ((MagikFlix)mContext.getApplicationContext()).getLocatyticsSession();
 		selectedStates = new Vector<Boolean>();
 		clearSelectedState();
 		selectedStates.set(((MagikFlix)context.getApplication()).getSelectedProfileIndex(), new Boolean(true));
-		enableCategoryWidthHeight = (int) mContext.getResources().getDimension(R.dimen.enabledCategoryItemWidthHeight);
 		disableCategoryWidthHeight = (int) mContext.getResources().getDimension(R.dimen.disabledCategoryItemWidthHeight);
 	}
 
@@ -71,19 +63,11 @@ public class ChildProfileAdapter extends BaseAdapter{
 			LayoutInflater inflater = mContext.getLayoutInflater();
 			rowView = inflater.inflate(R.layout.child_profile_list_item, null);
 			ViewHolder viewHolder = new ViewHolder();
-			viewHolder.movieThumbnailTV = (ImageView) rowView
-					.findViewById(R.id.child_profile_thumbnail_iv);
-			viewHolder.selectedIV = (ImageView) rowView
-					.findViewById(R.id.child_profile_slected_iv);
-			viewHolder.category_title_tv = (TextView) rowView
-					.findViewById(R.id.child_profile_title_tv);
-
-			//			viewHolder.mOuterCircleLayout = (RelativeLayout) rowView
-			//					.findViewById(R.id.category_outline_layout);
-			viewHolder.mInnerCircleLayout = (RelativeLayout) rowView
-					.findViewById(R.id.child_profile_inline_layout);
-			viewHolder.mCheckMarkIV = (ImageView) rowView
-					.findViewById(R.id.selection_check_mark);
+			viewHolder.movieThumbnailTV = (ImageView) rowView.findViewById(R.id.child_profile_thumbnail_iv);
+			viewHolder.selectedIV = (ImageView) rowView.findViewById(R.id.child_profile_slected_iv);
+			viewHolder.category_title_tv = (TextView) rowView.findViewById(R.id.child_profile_title_tv);
+			viewHolder.mInnerCircleLayout = (RelativeLayout) rowView.findViewById(R.id.child_profile_inline_layout);
+			viewHolder.mCheckMarkIV = (ImageView) rowView.findViewById(R.id.selection_check_mark);
 			rowView.setTag(viewHolder);
 		}
 
@@ -101,7 +85,6 @@ public class ChildProfileAdapter extends BaseAdapter{
 			public void onClick(View v) {
 				if(((FamilySetUpActivity)mContext).saveUserProfile(false, false)){
 					clearSelectedState();
-					//				sendLocalyticsForCategory(position);
 					selectedStates.set(position, new Boolean(true));
 					((FamilySetUpActivity)mContext).updateUI(position);
 					ChildProfileAdapter.this.notifyDataSetChanged();
@@ -117,56 +100,11 @@ public class ChildProfileAdapter extends BaseAdapter{
 			holder.selectedIV.setVisibility(View.GONE);
 			holder.mCheckMarkIV.setVisibility(View.GONE);
 		}
-
-
-		//		if(mPlayLists[position].thumbnailUrl != null && mPlayLists[position].thumbnailUrl.length() > 0)
-		//			Picasso.with(mContext).load(mPlayLists[position].thumbnailUrl).into(holder.movieThumbnailTV);
-		//		else
-		//			holder.movieThumbnailTV .setImageDrawable(mContext.getResources().getDrawable(R.drawable.icon_outline_bg));
-		//
-		//
-		//		if (selectedStates.get(position)){
-		//			holder.movieThumbnailTV.requestLayout();
-		//			holder.mInnerCircleLayout.requestLayout();
-
-
-
-		//			if(CompatibilityUtil.isTablet(mContext)){
-		//				holder.movieThumbnailTV.requestLayout();
-		//				holder.movieThumbnailTV.getLayoutParams().height = enableCategoryWidthHeight;
-		//				holder.movieThumbnailTV.getLayoutParams().width = enableCategoryWidthHeight;
-		//				holder.mInnerCircleLayout .getLayoutParams().height = enableCategoryWidthHeight;
-		//				holder.mInnerCircleLayout .getLayoutParams().width = enableCategoryWidthHeight;
-		//			}else{
-		//			holder.movieThumbnailTV.requestLayout();
-		//			holder.movieThumbnailTV.getLayoutParams().height = enableCategoryWidthHeight;
-		//			holder.movieThumbnailTV.getLayoutParams().width = enableCategoryWidthHeight;
-		//			holder.mInnerCircleLayout .getLayoutParams().height = enableCategoryWidthHeight;
-		//			holder.mInnerCircleLayout .getLayoutParams().width = enableCategoryWidthHeight;
-		//			//			}
-		//			holder.mInnerCircleLayout.setBackground(mContext.getResources().getDrawable(R.drawable.icon_outline_color_selected));
-		//		}
-		//		else {
-		//			holder.mInnerCircleLayout.requestLayout();
-		//			if(CompatibilityUtil.isTablet(mContext)){
-		//				holder.movieThumbnailTV.requestLayout();
-		//				holder.movieThumbnailTV.getLayoutParams().height = dpToPx(80);
-		//				holder.movieThumbnailTV.getLayoutParams().width = dpToPx(80);
-		//				holder.mInnerCircleLayout .getLayoutParams().height = dpToPx(80);
-		//				holder.mInnerCircleLayout .getLayoutParams().width = dpToPx(80);
-		//			}else{
 		holder.movieThumbnailTV.requestLayout();
 		holder.movieThumbnailTV.getLayoutParams().height = disableCategoryWidthHeight;
 		holder.movieThumbnailTV.getLayoutParams().width = disableCategoryWidthHeight;
 		holder.mInnerCircleLayout .getLayoutParams().height = disableCategoryWidthHeight;
 		holder.mInnerCircleLayout .getLayoutParams().width = disableCategoryWidthHeight;
-		//			}
-
-		//			holder.mInnerCircleLayout.setBackground(mContext.getResources().getDrawable(R.drawable.icon_outline_color_unselected));
-		//		}
-		//		holder.category_title_tv.setVisibility(View.GONE);
-		//
-		//		holder.category_title_tv.setText(mPlayLists[position].name);
 		return rowView;
 	}
 
